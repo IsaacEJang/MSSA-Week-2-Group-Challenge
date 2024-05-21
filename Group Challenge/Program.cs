c5150d2288b9563f1a613882c1040c80eae98c5c
eunta <euntak.jang@snhu.edu>
2024-05-21 09:01:31 -0700

    Add project files.

---
+++ Group Challenge/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Challenge
+{
+    internal class Program
+    {
+        static int BiggestNumber(int[] arr)
+        {
+            int max = arr[0];
+            int maxInt = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    maxInt = i;
+                }
+
+            }
+            return maxInt;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 10 numbers and I will tell you the max\n");
+
+
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{i + 1}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"\nThe max is {arr[BiggestNumber(arr)]} found at {BiggestNumber(arr) + 1}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
